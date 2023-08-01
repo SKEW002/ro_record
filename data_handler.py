48d335ac96fed794e3eb3a650209629ade6ea538
Shen Yu <shenyu@ventitechnologies.com>
2023-08-01 09:55:38 +0800

    Added function to update bug tickets

--- data_handler.py
+++ data_handler.py
@@ -15,43 +15,53 @@ class DataHandler:
 		return [dataframe.columns.tolist()] + dataframe.values.tolist()
 
 
-	def update_online(self,updated_data): # updated data in dict
-		title = datetime.datetime.now().strftime("%m%y"+"_History")
-		try:
-			history_data = self.__sh.worksheet(title)
-		except gspread.exceptions.WorksheetNotFound:
-			self.create_worksheet(title)
-			history_data = self.__sh.worksheet(title)
+	def update_online(self,function, updated_data): # updated data in dict
+		if function == "ro_record":
+			title = datetime.datetime.now().strftime("%m%y"+"_History")
+			try:
+				history_data = self.__sh.worksheet(title)
+			except gspread.exceptions.WorksheetNotFound:
+				self.create_worksheet(title)
+				history_data = self.__sh.worksheet(title)
 
-		# title = "Test History"
-		# history_data = self.__sh.worksheet(title)
-		history_dataframe = pd.DataFrame(history_data.get_all_records())
-		updated_dataframe = pd.DataFrame.from_dict(updated_data)
+			# title = "Test History"
+			# history_data = self.__sh.worksheet(title)
+			history_dataframe = pd.DataFrame(history_data.get_all_records())
+			updated_dataframe = pd.DataFrame.from_dict(updated_data)
 
-		history_dataframe = pd.concat([history_dataframe, updated_dataframe])
-		history_data.update("A1", self.dataframe_tolist(history_dataframe))
+			history_dataframe = pd.concat([history_dataframe, updated_dataframe])
+			history_data.update("A1", self.dataframe_tolist(history_dataframe))
 
-		compiled_data = self.__sh.worksheet("Test Compiled Data")
-		compiled_dataframe = pd.DataFrame(compiled_data.get_all_records())
-		compiled_dataframe_columns = list(compiled_dataframe.columns)
-		if compiled_dataframe.empty:
-			compiled_dataframe_columns = compiled_data.row_values(1)
-			compiled_dataframe = updated_dataframe.filter(items=compiled_dataframe_columns)
-			#compiled_dataframe = pd.concat([compiled_dataframe, updated_dataframe])
-
-		else:
-			if updated_data["RO Name"] in compiled_dataframe["RO Name"].unique():
-				index = compiled_dataframe.loc[compiled_dataframe["RO Name"] == updated_data["RO Name"]].index[0]
-				for column_name in compiled_dataframe_columns:
-					if column_name == "RO Name":
-						continue
-					compiled_dataframe.at[index, column_name] += updated_data[column_name]
+			compiled_data = self.__sh.worksheet("Test Compiled Data")
+			compiled_dataframe = pd.DataFrame(compiled_data.get_all_records())
+			compiled_dataframe_columns = list(compiled_dataframe.columns)
+			if compiled_dataframe.empty:
+				compiled_dataframe_columns = compiled_data.row_values(1)
+				compiled_dataframe = updated_dataframe.filter(items=compiled_dataframe_columns)
+				#compiled_dataframe = pd.concat([compiled_dataframe, updated_dataframe])
 
 			else:
-				updated_dataframe = updated_dataframe.filter(items=compiled_dataframe_columns)
-				compiled_dataframe = pd.concat([compiled_dataframe, updated_dataframe])
+				if updated_data["RO Name"] in compiled_dataframe["RO Name"].unique():
+					index = compiled_dataframe.loc[compiled_dataframe["RO Name"] == updated_data["RO Name"]].index[0]
+					for column_name in compiled_dataframe_columns:
+						if column_name == "RO Name":
+							continue
+						compiled_dataframe.at[index, column_name] += updated_data[column_name]
 
-		compiled_data.update("A1", self.dataframe_tolist(compiled_dataframe))
+				else:
+					updated_dataframe = updated_dataframe.filter(items=compiled_dataframe_columns)
+					compiled_dataframe = pd.concat([compiled_dataframe, updated_dataframe])
+
+			compiled_data.update("A1", self.dataframe_tolist(compiled_dataframe))
+
+		elif function == "bug_ticket":
+			try:
+				bug_data = self.__sh.worksheet(title)
+			except gspread.exceptions.WorksheetNotFound:
+				self.create_worksheet(title)
+				bug_data = self.__sh.worksheet(title)
+
+			history_data.update("A1", self.dataframe_tolist(history_dataframe))
 
 
 	def test_print(self):
