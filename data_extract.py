a4354c3fd25c21c3dae7462ad0e4ab892d52e927
Shen Yu <shenyu@ventitechnologies.com>
2023-08-07 22:19:19 +0800

    Allow user to select version for data extraction

--- data_extract.py
+++ data_extract.py
@@ -8,8 +8,10 @@ if __name__ == "__main__":
         interface = UI("Data Extract", 800, 850)
         interface.drop_down(list(filter(None, ro_data.ro_dataframe['RO Name'].tolist())), 'RO Name')
         interface.y += 30
+        interface.drop_down(list(filter(None, ro_data.ro_dataframe['Version'].tolist())), 'Version')
+        interface.y += 30
         interface.calendar()
-        interface.root.mainloop()
+        ro_data.show_data(interface.raw_data)
 
     except KeyboardInterrupt:
         pass
